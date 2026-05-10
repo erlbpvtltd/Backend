d4fe936ca24e0214b1ab3ff844fbeabf7200b989
erlbpvtltd <erlbpvtltd@gmail.com>
2026-05-10 13:29:00 +0530

    url updare

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -7,7 +7,7 @@ import Stripe from "stripe";
 const placeOrder = async (req, res) => {
     
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
-    const frontend_url = 'http://localhost:5173/'
+    const frontend_url = 'https://erlbpvtltd.onrender.com'
 
     try {
         const newOrder = new orderModel({
