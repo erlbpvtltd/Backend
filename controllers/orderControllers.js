80471a062f0672e40feca29ba35232ef5b95c6b8
erlbpvtltd <erlbpvtltd@gmail.com>
2026-05-10 12:51:31 +0530

    Update orderControllers.js

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -38,7 +38,7 @@ const placeOrder = async (req, res) => {
                 product_data:{
                     name:"Delivery Charges"
                 },
-                unit_amount:2.23*100*80
+                unit_amount:50
             },
             quantity: 1
         })
