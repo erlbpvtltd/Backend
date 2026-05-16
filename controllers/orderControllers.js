6093a0cc1b0386bb2d0f833ac44354ace31adc2f
erlbpvtltd <erlbpvtltd@gmail.com>
2026-05-16 23:19:06 +0530

    Update orderControllers.js

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -1,14 +1,11 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
-import Stripe from "stripe";
     
 
 // placing the user order on the frontend page
 const placeOrder = async (req, res) => {
     
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
-    const frontend_url = 'https://erlbpvtltd.onrender.com'
-
+    
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
@@ -19,41 +16,7 @@ const placeOrder = async (req, res) => {
         await newOrder.save(); // saving in the database 
 
 
-        // Sending the Amount in the INR to the stripe for the payment
-        const line_items = req.body.items.map((item)=>({
-            price_data:{
-                currency:"inr",
-                product_data:{
-                    name:item.name
-                },
-                unit_amount:item.price
-            },
-            quantity: item.quantity
-        }))
-
-        //also passing the delivery charges to the strip in INR to include in payment
-        line_items.push({
-            price_data:{
-                currency:"inr",
-                product_data:{
-                    name:"Delivery Charges"
-                },
-                unit_amount:50
-            },
-            quantity: 1
-        })
-
-        
-
-        //creating a url that tell weather the payment done or not
-        const session = await stripe.checkout.sessions.create({
-            line_items:line_items,
-            mode:"payment",
-            //remender that after the ? we send the query 
-            success_url:`${frontend_url}verify?success=true&orderId=${newOrder._id}`,
-            cancel_url:`${frontend_url}verify?success=false&orderId=${newOrder._id}`,
-        })
-        res.json({success:true, session_url:session.url})
+        res.json({success:true, message:"Order Placed Successfull!\n Soon you will get a conformation Call!! "})
 
     } catch (error) {
         console.log(error)
